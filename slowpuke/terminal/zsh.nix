e44e3bd9807334d17af4084e9d5ca478977ef718
slowpuke <slowpuke@tuta.io>
2024-10-05 16:55:11 +0200

    sn

--- slowpuke/terminal/zsh.nix
+++ slowpuke/terminal/zsh.nix
@@ -12,7 +12,6 @@
             };
         };
         shellAliases = {
-            pg = "bash ~/here/programming/bash/password-generator";
             ssd-update = "${config.home.homeDirectory}/does-nix/scripts/ssd-backup-notification/ssd-backup-notif update";
             lisp = "sbcl --script $1";
             cdnix = "cd ${config.home.homeDirectory}/does-nix";
@@ -21,6 +20,10 @@
             # aliases to create a nix shell file in current directory
             blankshell = "cp ${config.home.homeDirectory}/does-nix/flakes/blank-flake.nix ./flake.nix";
             luashell = "cp ${config.home.homeDirectory}/does-nix/flakes/lua-flake.nix ./flake.nix";
+            cobolshell = "cp ${config.home.homeDirectory}/does-nix/flakes/cobol-flake.nix ./flake.nix";
+            cshell = "cp ${config.home.homeDirectory}/does-nix/flakes/c-flake.nix ./flake.nix";
+            rustshell = "cp ${config.home.homeDirectory}/does-nix/flakes/rust-flake.nix ./flake.nix";
+            lispshell = "cp ${config.home.homeDirectory}/does-nix/flakes/lisp-flake.nix ./flake.nix";
         };
         envExtra = "
             export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
